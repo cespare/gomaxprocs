b456777cac5c6c01208a291a9bfc1729ee9d5289
Caleb Spare <cespare@gmail.com>
2013-10-21 17:17:37 -0700

    Docs and add SetToNumCPU

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 
 Gomaxprocs is a Go micro library that offers a slightly nicer interface to `runtime.GOMAXPROCS`.
 
-* `Get() int`: Returns the current setting for `GOMAXPROCS`
-* `Set(n int)`: Set a new value of `GOMAXPROCS`, but only if the `GOMAXPROCS` environment variable is not
-  already set.
+[Documentation can be viewed at godoc.org.](http://godoc.org/github.com/cespare/gomaxprocs)
--- gomaxprocs.go
+++ gomaxprocs.go
@@ -5,10 +5,18 @@ import (
 	"runtime"
 )
 
+// Get returns the current value of GOMAXPROCS.
 func Get() int { return runtime.GOMAXPROCS(-1) }
 
+// Set sets the value of GOMAXPROCS to n, unless the GOMAXPROCS environment variable was already set.
 func Set(n int) {
 	if os.Getenv("GOMAXPROCS") == "" {
 		runtime.GOMAXPROCS(n)
 	}
+}
+
+// SetToNumCPU sets GOMAXPROCS to the number of logical CPUs, unless the GOMAXPROCS environment variable was
+// already set.
+func SetToNumCPU() {
+	Set(runtime.NumCPU())
 }
